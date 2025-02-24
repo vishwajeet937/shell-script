89526fd2dab77c830e1a4c5debb43f6ff74b4905
vishwajeet kumar <155358120+vishwajeet937@users.noreply.github.com>
2025-02-24 16:19:25 +0530

    Create hello_chatur.sh

---
+++ hello_chatur.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "chatur: Adarniya sabhapati mahodaya"
+
+echo "chatur: aaj hum sab mil ke chamatkaar karenge"
+
+echo "rancho: ratta marega toh aisa hi hoga"
+
+echo "chatur: thuchuk thuchuk"
+
+echo "raju: rancho ithna hi tuje toh ek folder banake dikhana"
+
+mkdir -p rancho
+
+echo "rancho: ye leh, bana diya, ro mat ab"
+
+echo "farhan: viurus ne beizaati kar di, bola farhanitrate"
+
+echo "farhan: abba nhi manenge" > farhanitrate.txt
